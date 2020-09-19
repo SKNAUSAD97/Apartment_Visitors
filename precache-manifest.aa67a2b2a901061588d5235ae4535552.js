5f5d189a6b52cc2c727a34c3466d5e99548a9946
SKNAUSAD97 <sknausat97@gmail.com>
2020-09-19 22:47:30 +0530

    Updates

---
+++ precache-manifest.aa67a2b2a901061588d5235ae4535552.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d783004b2d6d0c0111c8647983414e39",
+    "url": "/Apartment_Visitors/index.html"
+  },
+  {
+    "revision": "03352985d318075af22b",
+    "url": "/Apartment_Visitors/static/js/2.953a9ca0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Apartment_Visitors/static/js/2.953a9ca0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1eda4c9ff81c656485",
+    "url": "/Apartment_Visitors/static/js/main.5bfa54c0.chunk.js"
+  },
+  {
+    "revision": "e7aae291019f991ee386",
+    "url": "/Apartment_Visitors/static/js/runtime-main.b204d189.js"
+  }
+]);
